f631c59d6e874bb38ad2a3b4ca353087b46662cb
JihyeonNam-0327 <n_jihyeon@naver.com>
2020-03-04 01:18:04 +0900

    BottomMenu Component 생성
    BottomMenu Component 생성
    ---
    - BottomMenu Component 생성
      - 중첩된 라우팅 사용
      - mail.js children 라우팅 옵션 사용
    - vue 파일 vbase 적용
    - Main 화면 생성 중

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <bottom-menu class="bottom-menu-component" @homeBtnClicked="pageMoveFunc" @mapBtnClicked="pageMoveFunc" @userBtnClicked="pageMoveFunc"></bottom-menu>
+  </div>
+</template>
+
+<script>
+  import BottomMenu from '@/components/BottomMenu'
+
+  export default {
+    components: {
+      BottomMenu
+    },
+    methods: {
+      pageMoveFunc(pageName) {
+        this.$router.replace({name: pageName, prams: {}})
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .bottom-menu-component {
+    position: absolute;
+    width: 100%;
+    bottom: 0px;
+  }
+</style>
