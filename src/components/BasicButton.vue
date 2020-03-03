0a2ed5957634e55b056e1aac66c1c08bf083dd77
JihyeonNam-0327 <n_jihyeon@naver.com>
2020-03-03 10:47:59 +0900

    files commit

---
+++ src/components/BasicButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="basic-button-outer">
+    <div class="button-outline">
+      <div class="text-box">
+        {{ buttonText }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      buttonText: {
+        type: String,
+        default: '로그인'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .basic-button-outer {
+    .button-outline {
+      width:100%;
+      height:42px;
+      background:#3C3C3C 0% 0% no-repeat padding-box;
+      opacity:1;
+      color: #ffffff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>main page</p>
+    <basic-button buttonText="회원가입"></basic-button>
+    <input-box class="input-component" @inputValue="getValue"></input-box>
+  </div>
+</template>
+
+<script>
+  import BasicButton from '../components/BasicButton.vue'
+  import InputBox from '../components/InputBox.vue'
+
+  export default {
+    components: {
+      BasicButton,
+      InputBox
+    },
+    methods: {
+      getValue(args) {
+        console.log(args);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
