e6512c00791cea3e724b899252c9673b43606536
jhj1390jhj <42240670+jhj1390jhj@users.noreply.github.com>
2020-03-05 20:58:05 +0900

    vutify install
    - vue add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,13 @@
-// 경로: 루트 디렉토리/vue.config.js
 const path = require('path');
-console.log('__dirname : ', __dirname);
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.join(__dirname, 'src/')
+  "configureWebpack": {
+    "resolve": {
+      "alias": {
+        "@": path.join(__dirname, 'src/')
       }
     }
-  }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
