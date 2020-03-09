90e06eb8b38d2e481e6818e6e359e35128084ca7
jhj1390jhj <42240670+jhj1390jhj@users.noreply.github.com>
2020-03-09 13:56:44 +0900

    create component tag & add icon
    - create component tag 생성 완료
    - src/assets/icon 추가(3)
    - page saerch 수정중

---
+++ src/components/Tag.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="tag-outer">
+
+    <div class=background>
+      <div class="tag-container">
+        <div class="tag-filter" :key="index" v-for="(item, index) in filterItem">
+            {{ item.filterName }}
+            <img src="@/assets/icon/icon_x.svg" alt="x">
+        </div>
+      </div>
+    </div>
+
+    <div class="tag-refresh">
+      <img src="@/assets/icon/icon_refresh.svg">
+    </div>
+
+   </div>
+</template>
+
+<script>
+  export default {
+    props: {
+    },
+    data() {
+      return {
+        filterItem: [
+          { filterName: '연남', status: 'active' },
+          { filterName: '주차가능', status: 'active' },
+          { filterName: '주차가능', status: 'active' },
+          { filterName: '주차가능', status: 'active' },
+          { filterName: '주차가능', status: 'active' },
+        ]
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tag-outer {
+    display: flex;
+    margin-left: 20px;
+    margin-right: 20px;
+    font-size: 15px;
+    .tag-container {
+        display: flex;
+        width: calc(100%-30px);
+        line-height: 30px;
+        vertical-align: middle;
+        overflow-x: scroll;
+        .tag-filter {
+          flex: 0 0 auto;
+          border: 1px solid black;
+          border-radius: 20px;
+          padding-left: 12px;
+          padding-right: 8px;
+          margin-right: 10px;
+          text-align: center;
+          img {
+            padding: 10px 5px 10px 7px;
+            vertical-align: middle;
+          }
+        }
+      }
+    .tag-refresh {
+      margin-left: 10px;
+      img {
+          padding: 10px 7px 10px 7px;
+          vertical-align: middle;
+      }
+    }
+  }
+</style>
--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -1,15 +1,107 @@
 <template>
-  <div>
+  <div class="search-outer">
+    <header-component class="header-component" leftType="historyback" centerType="underbar" rightType="search"></header-component>
+    <div class="search-tag-background">
+      <tag class="search-tag"></tag>
+    </div>
+
+    <div class="horizontal-line"></div>
+    <div class="filter-container">
+      <p>지역</p>
+      selectbox
+    </div>
+
+    <div class="horizontal-line"></div>
+    <div class="filter-container">
+      <p>카테고리</p>
+      selectbox
+    </div>
+
+    <div class="horizontal-line"></div>
+    <div class="filter-container">
+      <p>방문시간</p>
+      dial
+    </div>
+
+    <div class="horizontal-line"></div>
+    <div class="filter-container">
+      <p>체류시간</p>
+      dial
+    </div>
+
+    <div class="horizontal-line"></div>
+    <div class="filter-container">
+      <p>작업환경</p>
+      <toggle-button class="toggle" v-model="outlet" :width="45" :height="24" :color="{checked: '#37ABDA', unchecked: '#E5E5E5'}"/>
+    </div>
+
+    <div class="horizontal-line"></div>
+    <div class="filter-container">
+      <div>주차</div>
+      <toggle-button class="toggle" v-model="outlet" :width="45" :height="24" :color="{checked: '#37ABDA', unchecked: '#E5E5E5'}"/>
+    </div>
 
   </div>
 </template>
 
 <script>
+  import HeaderComponent from '@/components/HeaderComponent'
+  import Tag from '@/components/Tag'
+
   export default {
-    
+    data() {
+      return {
+        place: ['전체', '연남', '연희', '망원', '상수', '서교'],
+      }
+    },
+    components: {
+      HeaderComponent,
+      Tag,
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  .search-outer {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+    .horizontal-line {
+      width: 100%;
+      height: 1px;
+      margin-top: 6px;
+      margin-bottom: 6px;
+      background-color: #EBEBEB;
+    }
+    .header-component {
+      width: calc(100% - 16px);
+      position: absolute;
+      top: 36px;
+      left: 0px;
+    }
+    .search-tag-background {
+      background-color: #F7F7F7;
+      width: 100%;
+      height: 52px;
+      position: absolute;
+      top: 85px;
+      left: 0px;
+      .search-tag {
+        padding-top: 10px;
+        padding-bottom: 10px;
+      }
+    }
+    .filter-container {
+      display: flex;
+      width: calc(100% - 86px);
+      .toggle {
+        margin-left: auto;
+        margin-top: auto;
+      }
+    }
 
+  }
 </style>
