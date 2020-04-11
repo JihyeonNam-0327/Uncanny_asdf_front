c301ed87538c971c9840fdf4c6a3eeea55aa4fd5
jhj1390jhj <42240670+jhj1390jhj@users.noreply.github.com>
2020-04-11 18:33:16 +0900

    start
    start

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -4,9 +4,9 @@
     <header-component leftType="none" centerType="underbar" rightType="search"></header-component>
     <header-component leftType="none" centerType="underbar" rightType="home"></header-component>
     <select-box v-model="selectedValue"></select-box>
-  
-  
-  
+    <button @click="apiTest">버튼</button>
+    <p>{{userName}}</p>
+    
   </div>
 </template>
 
@@ -25,11 +25,28 @@
         slider: { 
           val: 10000, thumbColor: 'red', color: '#000000', trackColor: '#F7F7F7'
         },
+        userName: ''
       }
     },
     watch: {
       selectedValue(newVal) {
         console.log(newVal);
+      }
+    },
+    methods: {
+      apiTest() {
+        let params = {}
+        let token = {}
+        this.$api.menuList(params, token)
+        .then(response => {
+          console.log(response)
+          if(response.status === 200) {
+            this.userName = response.data
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
       }
     }
   }
