bb8f3f970cd6c2475010bfd6f1f5b05dfa70c5f1
JihyeonNam-0327 <n_jihyeon@naver.com>
2020-04-30 18:54:39 +0900

    정리

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
-    
+  <div class="flter-outer">
+    <span>{{}}</span>
   </div>
 </template>
 
 <script>
   export default {
-    
+    props: {
+      filter: {
+        type: Object,
+        // default: {
+        //   category: "운영시간",
+        //   option: [],
+        //   type: "check",
+        //   selected: []
+        // }
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-
+  .flter-outer {
+    height: 32px;
+    border: 0.5px solid #636363;
+    border-radius: 15px;
+  }
 </style>
