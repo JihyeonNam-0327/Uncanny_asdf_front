a0bc41396fa7c9436ef7a3e62d764311fe551077
jhj1390jhj <42240670+jhj1390jhj@users.noreply.github.com>
2020-05-25 14:33:14 +0900

    [생성] DetailInfo / vs-collapse 적용
    - StoreCard 분기
    DetailInfo - Detail / StoreCard >> mappage
    - DetailInfo 내 operatingHour : vs- collapse 적용

--- src/components/DetailInfo.vue
+++ src/components/DetailInfo.vue
@@ -1,310 +1,259 @@
 <template>
-  <div class="detail-info-outer">
-    <p class="bold">상호명</p>
-    <p>{{ storeInfo.nameKor }} / {{ storeInfo.nameEng }}</p>
-    <p class="bold">주소</p>
-    <p>{{ storeInfo.address }}</p>
-    <p class="bold">전화번호</p>
-    <p>{{ storeInfo.countryNum }} - {{ storeInfo.middleNum }} - {{ storeInfo.endNum }}</p>
+  <div class="store-card-outer">
 
-    <div class="line"></div>
-
-    <table class="table-basic">
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_coffee.svg"/></td>
-        <td class="td-title">아메리카노지수</td>
-        <td class="contents">{{ storeInfo.americanoIndex | priceFilter }}</td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_chair.svg"/></td>
-        <td class="td-title">좌석수</td>
-        <td class="contents">{{ storeInfo.totalDesk }}</td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_parking.svg"/></td>
-        <td class="td-title">주차</td>
-        <td class="contents"><img v-if="storeInfo.parking" src="@/assets/icon/icon_true.svg"/><img v-else src="@/assets/icon/icon_false.svg" /></td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_flag.svg"/></td>
-        <td class="td-title">대관 여부</td>
-        <td class="contents"><img v-if="storeInfo.rental" src="@/assets/icon/icon_true.svg"/><img v-else src="@/assets/icon/icon_false.svg" /></td>
-      </tr>
-    </table>
-
-    <div class="line"></div>
-
-    <p class="bold">작업환경</p>
-
-    <table>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_wifi.svg"/></td>
-        <td class="td-title">와이파이</td>
-        <td class="contents"><img v-if="storeInfo.wifi" src="@/assets/icon/icon_true.svg"/><img v-else src="@/assets/icon/icon_false.svg" /></td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_outlet.svg"/></td>
-        <td class="td-title">콘센트</td>
-        <td class="contents"><img v-if="storeInfo.outlet" src="@/assets/icon/icon_true.svg"/><img v-else src="@/assets/icon/icon_false.svg" /></td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_cube.svg"/></td>
-        <td class="td-title">공간감</td>
-        <td class="contents"><img v-if="storeInfo.space" src="@/assets/icon/icon_true.svg"/><img v-else src="@/assets/icon/icon_false.svg" /></td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_height.svg"/></td>
-        <td class="td-title">테이블높이</td>
-        <td class="contents"><img v-if="storeInfo.deskHeight" src="@/assets/icon/icon_true.svg"/><img v-else src="@/assets/icon/icon_false.svg" /></td>
-      </tr>
-    </table>
-
-    <div class="line"></div>
-
-    <div class="operation-time-text">
-      <p class="bold">운영시간</p>
-      <span class="holiday">{{ storeInfo.holiday }}</span>
+    <div class="picture-outer">
+      <div class="store-img" v-for="(storeImg, index) in storeCard.storeImgs" :key="index">
+        <img :src=storeImg @click="enlargementChange(index)">
+      </div>
+      <div class="add-photo-button">
+        <img src="@/assets/icon/icon_plus_circle2.svg" alt="">
+      </div>
     </div>
 
+    <div class="information-outer">
+      <div class="left-part">
+        <div class="line1">
+          <span class="title">{{ storeCard.storeNameKor }} </span> 
+          <span class="title">{{ storeCard.storeBranchKor }} </span>
+          <span>{{ storeCard.category }}</span>
+        </div>
+        <div class="line2">
+          <span>{{ storeCard.storeNameEng }} </span>
+          <span>{{ storeCard.storeBranchEng }}</span>
+        </div>
+        <div class="line2-1">
+          <div><span>{{ storeCard.address }}</span><span> {{ storeCard.floor }}</span></div>
+          <div>{{ storeCard.phoneNumber }}</div>
+        </div>
 
-    <table class="operation-time-table">
-      <tr>
-        <td></td>
-        <td>월</td>
-        <td>화</td>
-        <td>수</td>
-        <td>목</td>
-        <td>금</td>
-        <td>토</td>
-        <td>일</td>
-      </tr>
-      <tr>
-        <td>open</td>
-        <td>{{ storeInfo.openMon }}</td>
-        <td>{{ storeInfo.openTue }}</td>
-        <td>{{ storeInfo.openWed }}</td>
-        <td>{{ storeInfo.openThu }}</td>
-        <td>{{ storeInfo.openFri }}</td>
-        <td>{{ storeInfo.openSat }}</td>
-        <td>{{ storeInfo.openSun }}</td>
-      </tr>
-      <tr>
-        <td>close</td>
-        <td>{{ storeInfo.closeMon }}</td>
-        <td>{{ storeInfo.closeTue }}</td>
-        <td>{{ storeInfo.closeWed }}</td>
-        <td>{{ storeInfo.closeThu }}</td>
-        <td>{{ storeInfo.closeFri }}</td>
-        <td>{{ storeInfo.closeSat }}</td>
-        <td>{{ storeInfo.closeSun }}</td>
-      </tr>
-    </table>
 
-    <div class="line"></div>
+        <div class="line3">
+          <vs-collapse accordion class="operating-hour-collapse">
+            <vs-collapse-item>
+              <div slot="header">
+                <span class="openNow" v-if="storeCard.openClosed">
+                  Open Now</span>
+                <span class="closedNow" v-else>
+                  Closed Now</span>
+                <span v-if="!operatingHourToggle" >
+                  <span> · 영업 종료 </span>
+                  <span>{{ storeCard.closingHour }}</span>
+                </span>
+              </div>
 
-    <table>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_globe.svg"/></td>
-        <td class="title-short">URL</td>
-        <td class="contents long">{{ storeInfo.URL | textFilter}}</td>
-      </tr>
-      <tr>
-        <td class="icon"><img src="@/assets/icon/icon_instagram.svg"/></td>
-        <td class="title-short">instagram</td>
-        <td class="contents long">{{ storeInfo.instagram | textFilter}}</td>
-      </tr>
-    </table>
+              <div>
+                <span class="operating-title">휴무</span>
+                <span>{{ storeCard.holiday }}</span>
+              </div>
+              <div>
+                <span class="operating-title">월요일</span>
+                <span>{{ storeCard.openMon }}</span>
+                <span v-if="storeCard.openMon"> ~ </span>
+                <span v-else>휴무</span>
+                <span>{{ storeCard.closeMon }}</span>
+              </div>
+              <div>
+                <span class="operating-title">화요일</span>
+                <span>{{ storeCard.openTue }}</span>
+                <span v-if="storeCard.openTue"> ~ </span>
+                <span v-else>휴무</span>
+                <span>{{ storeCard.closeTue }}</span>
+              </div>
+              <div>
+                <span class="operating-title">수요일</span>
+                <span>{{ storeCard.openWed }}</span>
+                <span v-if="storeCard.openWed"> ~ </span>
+                <span v-else>휴무</span>
+                <span>{{ storeCard.closeWed }}</span>
+              </div>
+              <div>
+                <span class="operating-title">목요일</span>
+                <span>{{ storeCard.openThu }}</span>
+                <span v-if="storeCard.openThu"> ~ </span>
+                <span v-else>휴무</span>
+                <span>{{ storeCard.closeThu }}</span>
+              </div>
+              <div>
+                <span class="operating-title">금요일</span>
+                <span>{{ storeCard.openFri }}</span>
+                <span v-if="storeCard.openFri"> ~ </span>
+                <span v-else>휴무</span>
+                <span>{{ storeCard.closeFri }}</span>
+              </div>
+              <div>
+                <span class="operating-title">토요일</span>
+                <span>{{ storeCard.openSat }}</span>
+                <span v-if="storeCard.openSat"> ~ </span>
+                <span v-else>휴무</span>
+                <span>{{ storeCard.closeSat }}</span>
+              </div>
+              <div>
+                <span class="operating-title">일요일</span>
+                <span>{{ storeCard.openSun }}</span>
+                <span v-if="storeCard.openSun"> ~ </span>
+                <span v-else>휴무</span>
+              <span>{{ storeCard.closeSun }}</span>
+              </div>
 
-    <div class="map"> 
-      <naver-maps
-        :height="height"
-        :width="width"
-        :mapOptions="mapOptions"
-        :initLayers="initLayers"
-        @load="onLoad">
-        <naver-marker :lat="storeInfo.x" :lng="storeInfo.y" @click="onMarkerClicked" @load="onMarkerLoaded"></naver-marker>
-      </naver-maps>
+            </vs-collapse-item>
+          </vs-collapse>
+        </div>
+
+
+        <div class="line4">
+          <span><img src="@/assets/icon/Icon_seats_cnt.svg"/></span>
+          <span> {{ storeCard.seatsCnt }} · </span>
+          <span><img src="@/assets/icon/Icon_review_cnt.svg"/></span>
+          <span> {{ storeCard.reviewCnt }} · </span>
+          <span><img src="@/assets/icon/Icon_heart_cnt.svg"/></span>
+          <span> {{ storeCard.pinCnt }}</span>
+        </div>
+      </div>
+
+      <div class="right-part">
+        <div class="store-card-edit-button">
+          수정
+        </div>
+        <div class="distance-pin">
+          <div class="distance">{{ storeCard.distance }} km</div>
+          <div class="pin" @click="pinChange">
+            <div class="pin-img" v-if="storeCard.heart">
+              <img src="@/assets/icon/Icon_heart_fill_pin.svg"/></div>
+            <div class="pin-img" v-else>
+              <img src="@/assets/icon/Icon_heart_empty_pin.svg"/></div>
+          </div>
+        </div>
+      </div>
     </div>
+    <div class="horizontal-line"></div>
+
   </div>
 </template>
 
 <script>
   export default {
     props: {
-      storeInfo: {
-        type: Object,
-        default: () => {
-          return {
-            nameKor: '앤트러사이트서교',
-            nameEng: 'Anthracite Seogyo',
-            address: '서울 마포구 월드컵로12길 11 1층~3층',
-            countryNum: '02',
-            middleNum: '322',
-            endNum: '7009',
-            americanoIndex: 4500,
-            totalDesk: 30,
-            parking: false,
-            rental: false,
-            wifi: true,
-            outlet: true,
-            space: true,
-            deskHeight: true,
-            holiday: '매주 월요일 휴무',
-            openMon: '10:00',
-            closeMon: '22:00',
-            openTue: '10:00',
-            closeTue: '22:00',
-            openWed: '10:00',
-            closeWed: '22:00',
-            openThu: '10:00',
-            closeThu: '22:00',
-            openFri: '10:00',
-            closeFri: '22:00',
-            openSat: '10:00',
-            closeSat: '22:00',
-            openSun: '10:00',
-            closeSun: '22:00',
-            URL: '',
-            instagram: '@pouringlg_outl',
-            x: 37.555467,
-            y: 126.911652
-          }
-        }
+      storeCard: {
       },
-      mapOptions: {
-        type: Object,
-        default: () => {
-          return {
-            lat: 37.555467,
-            lng: 126.911652,
-            zoom: 15,
-            zoomControl: true,
-            zoomControlOptions: {position: 'TOP_RIGHT'},
-            mapTypeControl: true
-          }
-        }
-      }
-    },
-    filters: {
-      priceFilter(val) {
-        let num = new Number(val);
-        return num.toFixed(0).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,") + " 원"
-      },
-      textFilter(val) {
-        if(val) {
-          return val
-        } else {
-          return '-'
-        }
-      }
-    },
-    computed: {
-      hello() {
-        return `Hello, World! × ${this.count}`;
-      },
-      width() {
-        return window.innerWidth - 60
-      }
     },
     data() {
       return {
-        height: 250,
-        info: true,
-        marker: null,
-        count: 1,
-        map: null,
-        initLayers: ['BACKGROUND', 'BACKGROUND_DETAIL', 'POI_KOREAN', 'TRANSIT']
+        operatingHourToggle: false,
+        enlargement: false,
       }
     },
     methods: {
-      onLoad(vue) {
-        this.map = vue;
+      pinChange() {
+        this.$emit('pinChange')
       },
-      onMarkerClicked(event) {
-        console.log('event in onMarkerClicked: ', event);
-        this.info = !this.info;
+      operatingHourToggleChange() {
+        this.operatingHourToggle = !this.operatingHourToggle
       },
-      onMarkerLoaded(vue) {
-        this.marker = vue.marker
-        vue.marker.setClickable(true);
-      },
-      onWindowLoad(that) {
-        console.log(that);
-      },
-    },
-    mounted() {
-      setInterval(() => this.count++, 1000);
+      enlargementChange(index) {
+        this.$emit('enlargementChange', index)
+      }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  .detail-info-outer {
-    -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
-    -moz-box-sizing: border-box;    /* Firefox, other Gecko */
-    box-sizing: border-box;         /* Opera/IE 8+ */
-    width: 100%;
-    padding: 20px 30px 0px 30px;
-    table {
-      padding:0; 
-      border-spacing:0px; 
-      border:0; 
-      border-collapse:collapse;
-      font-size: 13px;
-      line-height: 21px;
-      width: 100%;
-      .icon {
-        width: 25px;
-        text-align: center;
-      }
-      .td-title {
-        width: 155px;
-      }
-      .title-short {
-        width: 70px;
-      }
-      .contents {
-        width: auto;
-      }
+  .store-card-outer {
+    font-size: 12px;
+
+    .horizontal-line {
+      height: 10px;
+      background-color: #EBEBEB;
     }
-    p {
-      font-size: 13px;
-      line-height: 21px;
-      padding-left: 5px;
-      margin: 0;
-    }
-    .bold {
-      font-weight: bold;
-    }
-    .line {
-      width: 100%;
-      height: 0px;
-      border: 0.5px solid #C3C3C3;
-      margin: 12px auto;
-    }
-    
-    .operation-time-text {
+
+    .picture-outer {
       display: flex;
-      p {
-        margin-right: auto;
+      overflow-y: scroll;
+      margin: 15px;
+
+      .store-img img {
+        width: 100px;
+        height: 100px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center center;
+        margin-right: 10px;
       }
-      .holiday {
-        text-align: right;
-        margin-left: auto;
-        font-size: 13px;
-        line-height: 21px;
-        letter-spacing: 0;
-        color: #DB2E2F;
+      .add-photo-button {
+        min-width: 100px;
+        height: 100px;
+        background-color: gray;
+        img {
+          display: block;
+          margin: 35px auto;
+       }
       }
     }
-    .operation-time-table {
-      margin-top: 5px;
-      text-align: center;
-      background: #F7F7F794 0% 0% no-repeat padding-box;
+    .picture-outer::-webkit-scrollbar {
+      display: none; /* Chrome, Safari, Opera*/
     }
-    .map {
-      margin-top: 25px;
-      margin-bottom: 20px;
+    .information-outer {
+      display: flex;
+      padding: 0px 15px 15px 15px;
+      .left-part {
+        width: calc(100vw - 30px);
+        .title {
+          font-size: 16px;
+        }
+        .line3 {
+          font-size: 12px;
+          .hearder {
+            display: inline-block;
+            width:150px;
+          }
+          .openNow {
+            color: #D02121;
+            font-weight: bold;
+          }
+          .closedNow {
+            color: grey;
+            font-weight: bold;
+          }
+          span div div .operating-title[data-v-4fd05711] {
+            display: inline-block;
+            width: 65px;
+          }
+          .open-button {
+            padding: 0px 10px 0px 10px;
+          }
+          .close-button {
+            padding: 0px 10px 0px 10px;
+          }
+        }
+        .line4 {
+          padding-top:2px;
+          span {
+            img {
+              vertical-align: -7%;
+            }
+          }
+        }
+      }
+      .right-part {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        width: 30vw;
+        .store-card-edit-button {
+          align-self: flex-end;        
+        }
+        .distance-pin {
+          display: flex;
+          justify-content: flex-end;
+          .distance {
+            padding: 14px 10px 0px 0px;
+          }
+          .pin {
+            .pin-img {
+              display: flex;
+            }
+          }
+        }
+      }
     }
   }
 </style>
--- src/pages/jhjTest.vue
+++ src/pages/jhjTest.vue
@@ -4,14 +4,11 @@
 
     <div class="horizontal-line"></div>
 
-    <store-card 
+    <detail-info
       v-bind:storeCard="storeCards"
-      detailInformation
-      operatingHourArrow
-      addPhotoButton
       @pinChange="pinChange"
       @enlargementChange="enlargementSet"
-    ></store-card>
+    ></detail-info>
     
     <div class="enlargement-outer" v-if="enlargementToggle" @click="enlargementCancle">
       <div class="enlargement-content">
@@ -61,13 +58,13 @@
 </template>
 
 <script>
-  import StoreCard from '@/components/StoreCard'
+  import DetailInfo from '@/components/DetailInfo'
   import DetailFilter from '@/components/DetailFilter'
   import { slider, slideritem } from 'vue-concise-slider'
 
   export default {
     components: {
-      StoreCard,
+      DetailInfo,
       DetailFilter,
       slider,
       slideritem,
